1da90cbaffbe2477f64272ba91c44372c0c1cd0e
RC_Student_lab <RC_Student_lab@10.0.0.128>
2024-09-29 07:36:24 +0000

    2nd attempt , created checkusername and checkpasswordcomplexity method

--- src/main/java/com/mycompany/mypoe/LOGIN.java
+++ src/main/java/com/mycompany/mypoe/LOGIN.java
@@ -11,19 +11,49 @@ import java.util.Scanner;
  */
 public class LOGIN {
    //import object scanner
-
+    Scanner sc = new Scanner(System.in);
     //Declarations
     String username;
     String password;
     String name;
     String lastname;
-    //prompt user to enter name and lastname
+   
+    
+    //prompt the user to enter name and lastname
+    public void Prompt(){
+        System.out.println("Enter name: ");
+        name = sc.next();
+        System.out.println("Enter lastname: ");
+        lastname = sc.next();
+    }
+     //creating a method
+    public boolean checkUserName(){
+        //prompt the user to enter username
+         System.out.println("enter username: ");
+        username = sc.next();
+        System.out.println(username.contains("_"));
+        System.out.println(username.length ()<= 5);
 
-
-public boolean checkUsername(){
-    Scanner sc = new Scanner(System.in);
-
-System.out.println("Enter name: ");
-name = sc.next();
-}
+         return true;
+    }
+        public boolean checkPasswordComplexity(){
+        //prompt the user to enter password
+       System.out.println("Enter password: ");
+        password = sc.next(); 
+        
+        String regex = "^(?=.*[0-9])"
+                       + "(?=.*[A-Z])"
+                       + "(?=.*[@#$%^&+=])";
+        if(password.length()>=8 && password.matches(regex)){
+              return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    
+    
+    
+    
 }
