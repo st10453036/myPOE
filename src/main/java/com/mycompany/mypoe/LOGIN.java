85545e16e98e8b9eb9a0459756dc4ce1fe3b1ebe
RC_Student_lab <RC_Student_lab@10.0.0.128>
2024-09-29 07:14:05 +0000

    FIRST ATTEMPT AFTER ROUGH DRAFT

---
+++ src/main/java/com/mycompany/mypoe/LOGIN.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mypoe;
+import java.util.Scanner;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class LOGIN {
+   //import object scanner
+
+    //Declarations
+    String username;
+    String password;
+    String name;
+    String lastname;
+    //prompt user to enter name and lastname
+
+
+public boolean checkUsername(){
+    Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter name: ");
+name = sc.next();
+}
+}
---
+++ src/main/java/com/mycompany/mypoe/MyPOE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mypoe;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class MyPOE {
+
+    public static void main(String[] args) {
+        System.out.println("Enter name: ");
+    }
+}
