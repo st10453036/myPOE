93ce859c05e0536c8376166f4ca2b6fa59caf1c4
RC_Student_lab <RC_Student_lab@10.0.0.128>
2024-09-30 18:46:11 +0000

    7th attempt

--- src/main/java/com/mycompany/mypoe/LOGIN.java
+++ src/main/java/com/mycompany/mypoe/LOGIN.java
@@ -9,7 +9,12 @@ import java.util.regex.*;
  *
  * @author RC_Student_lab
  */
-public class LOGIN {
+public class LOGIN{
+    public LOGIN(String username, String password){
+        this.username = username;
+this.password = password;
+    }
+
    //import object scanner
   
     //Declarations
@@ -31,10 +36,10 @@ public class LOGIN {
    }
     
      //creating a method
-    public boolean checkUserName(){
+    public boolean checkUserName(String username){
       return username.contains("_") && username.length ()<= 5;
     }
-        public boolean checkPasswordComplexity(){
+        public boolean checkPasswordComplexity(String password){
       
       
         String regex = "^(?=.*[0-9])"
@@ -53,41 +58,35 @@ public class LOGIN {
         return m.matches();
       
     }
-      public void display(String name, String lastname){
-            System.out.println("the username =  " + username);
-            System.out.println("the password = " + password);
+    
+          public String registerUser(String username , String password){
+          if (checkUserName(username) && checkPasswordComplexity(password)){
+              
+              return "The username is successfully captured and Password is successfully captured";
+          }
+         else{
+           return "The username is incorrectly formatted, please ensure that the username contains an underscore and is no more than 5 characters in length or Password is incorrectly formatted, please ensure that the password contains at least 8 chatacters,a capital letter ,a number and a special character";
+}
+        
+          }    
+             public boolean loginUser(String userName , String Password){
+          return this.username.equals(userName) && this.password.equals(Password);
+        }
+    
+          public String returnLoginStatus(String userName , String Password, String name, String lastname){
+                 if (loginUser(userName , Password)){
+               return "Welcome " + name + " "  + lastname +" " + "it is great to see you again";  
+            }
+            else{
+                      return "Username or password is incorrect.Please try again!";
+            }
+        
+        
+          }
+  public void display(String name, String lastname,String userName , String Password){
+            System.out.println("the username =  " + userName);
+            System.out.println("the password = " + Password);
             System.out.println("name = " + name);
             System.out.println("lastname =" + lastname);
         }
-          public String registerUser(String username , String password){
-          if (!checkUserName() && !checkPasswordComplexity()){
-              
-              System.out.println("The username is successfully captured and Password is successfully captured ");
-              return "true";
-          }
-         else{
-           System.out.println("The username is incorrectly formatted, please ensure that the username contains an underscore and is no more than 5 characters in length or Password is incorrectly formatted, please ensure that the password contains at least 8 chatacters,a capital letter ,a number and a special character");
-}
-        return "false";
-          }    
-             public boolean loginUser(String username , String password){
-            if(username.equals(username) && password.equals(password)){
-               return true; 
-            }
-            else{
-                return false;
-            }
-        }
-    
-          public String returnLoginStatus(String username , String password, String name, String lastname){
-                 if (loginUser(username , password) == true){
-               System.out.println("Welcome " + name + " "  + lastname +" " + "it is great to see you again");  
-            }
-            else{
-                      System.out.println("Username or password is incorrect.Please try again!");
-            }
-        return null;
-        
-        
-          }
         }
--- src/main/java/com/mycompany/mypoe/MyPOE.java
+++ src/main/java/com/mycompany/mypoe/MyPOE.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.mypoe;
-
+import java.util.Scanner;
 /**
  *
  * @author RC_Student_lab
@@ -11,15 +11,48 @@ package com.mycompany.mypoe;
 public class MyPOE {
 
     public static void main(String[] args) {
-       
-        LOGIN ab = new LOGIN();
-        ab.Prompt();
-        ab.checkUserName();
-        ab.checkPasswordComplexity();
-        ab.registerUser();
-        ab.loginUser();
-        ab.returnLoginStatus();
-        ab.display();
-       
+        Scanner sc = new Scanner(System.in);
+         Scanner cv = new Scanner(System.in);
+      
+        
+        //Declarations
+        String name;
+        String lastname;
+         String username;
+      String password;
+    
+      //prompt the user to enter name and lastname
+         System.out.println("Enter your name: ");
+        name = sc.next();
+        System.out.println("Enter your lastname: ");
+        lastname = sc.next();
+       //prompt user to enter username
+        System.out.println("enter username: ");
+       username = sc.next();
+        //prompt user to enter password
+         System.out.println("Enter password: ");
+         password = sc.next(); 
+         
+         //calling second class
+           LOGIN ab = new LOGIN(username,password);
+      
+     
+       //calling register user method
+        System.out.println( ab.registerUser(username , password));
+        
+        if (ab.checkPasswordComplexity(password) && ab.checkUserName(username)){
+            //prompt the user to enter password and username
+            System.out.println("Enter username");
+          String  userName = cv.next();
+            System.out.println("Enter password");
+            String Password = cv.next();
+            //calling the return login status method
+            System.out.println(ab.returnLoginStatus( userName ,Password, name,lastname));
+            //called the display method
+             ab.display( name ,lastname, userName,Password);
+        }
+        ab.loginUser(username , password);
+        
+    
     }
 }
