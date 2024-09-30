3d0bac3bc9aee9c5edddda710367e809e289c535
RC_Student_lab <RC_Student_lab@10.0.0.128>
2024-09-30 14:35:56 +0000

    6th attempt

--- src/main/java/com/mycompany/mypoe/LOGIN.java
+++ src/main/java/com/mycompany/mypoe/LOGIN.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.mypoe;
-import java.util.Scanner;
+
 import java.util.regex.*;
 /**
  *
@@ -11,36 +11,32 @@ import java.util.regex.*;
  */
 public class LOGIN {
    //import object scanner
-    Scanner sc = new Scanner(System.in);
+  
     //Declarations
-    String username;
-    String password;
-    String name;
-    String lastname;
+    private String username;
+    private String password;
    
-    
-    //prompt the user to enter name and lastname
-    public void Prompt(){
-        System.out.println("Enter name: ");
-        name = sc.next();
-        System.out.println("Enter lastname: ");
-        lastname = sc.next();
+   
+    public void setUserName(String username){
+        this.username = username;
     }
+   public String getUserName(){
+       return username;
+   }
+   public String getPassword(){
+       return password;
+   }
+   public void setPassword(String password){
+       this.password = password;
+   }
+    
      //creating a method
     public boolean checkUserName(){
-      
-        //prompt the user to enter username
-         System.out.println("enter username: ");
-        username = sc.next();
-        System.out.println(username.contains("_"));
-        System.out.println(username.length ()<= 5);
-
-         return true;
+      return username.contains("_") && username.length ()<= 5;
     }
         public boolean checkPasswordComplexity(){
-        //prompt the user to enter password
-       System.out.println("Enter password: ");
-        password = sc.next(); 
+      
+      
         String regex = "^(?=.*[0-9])"
                        + "(?=.*[A-Z])(?=.*[a-z])"
                        + "(?=.*[@#$%^&+=]).{8,}$";
@@ -57,23 +53,24 @@ public class LOGIN {
         return m.matches();
       
     }
-      public void display(){
+      public void display(String name, String lastname){
             System.out.println("the username =  " + username);
             System.out.println("the password = " + password);
             System.out.println("name = " + name);
             System.out.println("lastname =" + lastname);
         }
-          public String registerUser(){
-          if (checkUserName() ==true && checkPasswordComplexity()==true){
+          public String registerUser(String username , String password){
+          if (!checkUserName() && !checkPasswordComplexity()){
+              
               System.out.println("The username is successfully captured and Password is successfully captured ");
-          
+              return "true";
           }
          else{
            System.out.println("The username is incorrectly formatted, please ensure that the username contains an underscore and is no more than 5 characters in length or Password is incorrectly formatted, please ensure that the password contains at least 8 chatacters,a capital letter ,a number and a special character");
 }
-        return null;
+        return "false";
           }    
-             public boolean loginUser(){
+             public boolean loginUser(String username , String password){
             if(username.equals(username) && password.equals(password)){
                return true; 
             }
@@ -82,10 +79,9 @@ public class LOGIN {
             }
         }
     
-          public String returnLoginStatus(){
-                 if (loginUser() == true){
-               
-                     System.out.println("Welcome " + name + " "  + lastname +" " + "it is great to see you again");
+          public String returnLoginStatus(String username , String password, String name, String lastname){
+                 if (loginUser(username , password) == true){
+               System.out.println("Welcome " + name + " "  + lastname +" " + "it is great to see you again");  
             }
             else{
                       System.out.println("Username or password is incorrect.Please try again!");
